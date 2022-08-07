3cf206aca048f0bec7caa6b85b10e9227b7b9f08
Stewart Noll <stewartnoll@gmail.com>
2022-08-06 19:08:13 -0500

    express and supertest

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import env from "./config";
+
+const app = express();
+
+app.get("/posts", async (req, res) => {
+  const authorId = Number.parseInt(req.query.authorId as string);
+
+  const client = new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
+  });
+
+  const posts = await client.post.findMany({
+    select: {
+      author: {
+        select: {
+          name: true,
+        },
+      },
+      title: true,
+      content: true,
+      published: true,
+    },
+    where: {
+      authorId,
+    },
+  });
+
+  res.status(200).json({ data: posts });
+});
+
+export default app;
